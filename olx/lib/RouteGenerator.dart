c767c5165710c31f21525cb140f4e3b705707479
JrDeleyFerreira <deleyalemao@gmail.com>
2021-10-04 01:15:29 -0300

    Trabalhando com MenuDropDown, Carousel, isolando classes genéricas customizadas.

---
+++ olx/lib/RouteGenerator.dart
@@ -0,0 +1,60 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:olx/views/Anuncios.dart';
+import 'package:olx/views/DetalhesAnuncio.dart';
+import 'package:olx/views/Login.dart';
+import 'package:olx/views/MeusAnuncios.dart';
+import 'package:olx/views/NovoAnuncio.dart';
+
+class RouteGenerator {
+
+  static Route<dynamic> generateRoute(RouteSettings settings){
+
+    final args = settings.arguments;
+
+    switch( settings.name ){
+      case "/" :
+        return MaterialPageRoute(
+          builder: (_) => Anuncios()
+        );
+      case "/login" :
+        return MaterialPageRoute(
+            builder: (_) => Login()
+        );
+      case "/meus-anuncios" :
+        return MaterialPageRoute(
+            builder: (_) => MeusAnuncios()
+        );
+      case "/novo-anuncio" :
+        return MaterialPageRoute(
+            builder: (_) => NovoAnuncio()
+        );
+      case "/detalhes-anuncio" :
+        return MaterialPageRoute(
+            builder: (_) => DetalhesAnuncio(args)
+        );
+      default:
+        _erroRota();
+    }
+
+  }
+
+  static Route<dynamic> _erroRota(){
+
+    return MaterialPageRoute(
+      builder: (_){
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Tela não encontrada!"),
+          ),
+          body: Center(
+            child: Text("Tela não encontrada!"),
+          ),
+        );
+      }
+    );
+
+  }
+
+}
---
+++ olx/lib/views/MeusAnuncios.dart
@@ -0,0 +1,152 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:olx/models/Anuncio.dart';
+import 'package:olx/views/widgets/ItemAnuncio.dart';
+
+class MeusAnuncios extends StatefulWidget {
+  @override
+  _MeusAnunciosState createState() => _MeusAnunciosState();
+}
+
+class _MeusAnunciosState extends State<MeusAnuncios> {
+  final _controller = StreamController<QuerySnapshot>.broadcast();
+  String _idUsuarioLogado;
+
+  _recuperaDadosUsuarioLogado() {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User usuarioLogado = auth.currentUser;
+    _idUsuarioLogado = usuarioLogado.uid;
+  }
+
+  Future<Stream<QuerySnapshot>> _adicionarListenerAnuncios() async {
+    await _recuperaDadosUsuarioLogado();
+
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    Stream<QuerySnapshot> stream = db
+        .collection("meus_anuncios")
+        .doc(_idUsuarioLogado)
+        .collection("anuncios")
+        .snapshots();
+
+    stream.listen((dados) {
+      _controller.add(dados);
+    });
+  }
+
+  _removerAnuncio(String idAnuncio) {
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    db
+        .collection("meus_anuncios")
+        .doc(_idUsuarioLogado)
+        .collection("anuncios")
+        .doc(idAnuncio)
+        .delete()
+        .then((_) {
+      db.collection("anuncios").doc(idAnuncio).delete();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _adicionarListenerAnuncios();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var carregandoDados = Center(
+      child: Column(
+        children: <Widget>[
+          Text("Carregando anúncios"),
+          CircularProgressIndicator()
+        ],
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meus Anúncios"),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        foregroundColor: Colors.white,
+        icon: Icon(Icons.add),
+        label: Text("Adicionar"),
+        //child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.pushNamed(context, "/novo-anuncio");
+        },
+      ),
+      body: StreamBuilder(
+        stream: _controller.stream,
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return carregandoDados;
+              break;
+            case ConnectionState.active:
+            case ConnectionState.done:
+
+              //Exibe mensagem de erro
+              if (snapshot.hasError) return Text("Erro ao carregar os dados!");
+
+              QuerySnapshot querySnapshot = snapshot.data;
+
+              return ListView.builder(
+                  itemCount: querySnapshot.docs.length,
+                  itemBuilder: (_, indice) {
+                    List<DocumentSnapshot> anuncios =
+                        querySnapshot.docs.toList();
+                    DocumentSnapshot documentSnapshot = anuncios[indice];
+                    Anuncio anuncio =
+                        Anuncio.fromDocumentSnapshot(documentSnapshot);
+
+                    return ItemAnuncio(
+                      anuncio: anuncio,
+                      onPressedRemover: () {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Text("Confirmar"),
+                                content:
+                                    Text("Deseja realmente excluir o anúncio?"),
+                                actions: <Widget>[
+                                  FlatButton(
+                                    child: Text(
+                                      "Cancelar",
+                                      style: TextStyle(color: Colors.grey),
+                                    ),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                  FlatButton(
+                                    color: Colors.red,
+                                    child: Text(
+                                      "Remover",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                    onPressed: () {
+                                      _removerAnuncio(anuncio.id);
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                ],
+                              );
+                            });
+                      },
+                    );
+                  });
+          }
+
+          return Container();
+        },
+      ),
+    );
+  }
+}
