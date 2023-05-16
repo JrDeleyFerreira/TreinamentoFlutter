13d2f421b921043ee63759ba3d4204c517ecd59b
JrDeleyFerreira <deleyalemao@gmail.com>
2023-05-16 00:09:47 -0300

    Mudança SDK 3.10 do Dart/Flutter.

--- olx/lib/main.dart
+++ olx/lib/main.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:olx/RouteGenerator.dart';
 import 'package:olx/views/Anuncios.dart';
-import 'package:olx/views/Login.dart';
 
-final ThemeData temaPadrao = ThemeData(
-  primaryColor: Color(0xff9c27b0),
-  accentColor: Color(0xff7b1fa2)
-);
+final ThemeData temaPadrao =
+    ThemeData(primaryColor: Color(0xff9c27b0), hintColor: Color(0xff7b1fa2));
 
 void main() => runApp(MaterialApp(
-  title: "OLX",
-  home: Anuncios(),
-  theme: temaPadrao,
-  initialRoute: "/",
-  onGenerateRoute: RouteGenerator.generateRoute,
-  debugShowCheckedModeBanner: false,
-));
-
+      title: "OLX",
+      home: Anuncios(),
+      theme: temaPadrao,
+      initialRoute: "/",
+      onGenerateRoute: RouteGenerator.generateRoute,
+      debugShowCheckedModeBanner: false,
+    ));
--- whastapp/lib/main.dart
+++ whastapp/lib/main.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'Home.dart';
 import 'Login.dart';
 import 'RouteGenerator.dart';
 import 'dart:io';
 
-final ThemeData temaIOS = ThemeData(
-    primaryColor: Colors.grey[200],
-    accentColor: Color(0xff25D366)
-);
+final ThemeData temaIOS =
+    ThemeData(primaryColor: Colors.grey[200], hintColor: Color(0xff25D366));
 
 final ThemeData temaPadrao = ThemeData(
-    primaryColor: Color(0xff075E54),
-    accentColor: Color(0xff25D366),
+  primaryColor: Color(0xff075E54),
+  hintColor: Color(0xff25D366),
 );
 
-void main(){
+void main() {
   runApp(MaterialApp(
     home: Login(),
     theme: Platform.isIOS ? temaIOS : temaPadrao,
@@ -23,6 +19,4 @@ void main(){
     onGenerateRoute: RouteGenerator.generateRoute,
     debugShowCheckedModeBanner: false,
   ));
-
 }
-
